3d7ca3059e5fa7296bbac93952d424f3c86269eb
Valeriy <leshikzoloto@gmail.com>
2021-09-14 15:44:31 +0300

    ads video-fashion

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ function scripts() {
 		"node_modules/swiper/swiper-bundle.min.js",
 		"node_modules/slick-carousel/slick/slick.js",
 		"node_modules/mixitup/dist/mixitup.js",
-		"node_modules/fancybox/dist/js/jquery.fancybox.js",
+		"node_modules/fancybox/dist/js/fancybox.umd.js",
 		"node_modules/jquery-form-styler/dist/jquery.formstyler.js",
 		"node_modules/ion-rangeslider/js/ion.rangeSlider.js",
 		"./app/js/main.js",
