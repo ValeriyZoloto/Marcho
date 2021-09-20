a22fee9b68516bc3a896dfba3b548cfb8a025c32
Valeriy <leshikzoloto@gmail.com>
2021-09-20 17:12:48 +0300

    add shop__filters

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,10 @@ $(function () {
 	$(".filter-price__input").ionRangeSlider({
 		type: "double",
 		prefix: "$",
+		onStart: function (data) {
+			$(".filter-price__from").text(data.from);
+			$(".filter-price__to").text(data.to);
+		},
 		onChange: function (data) {
 			$(".filter-price__from").text(data.from);
 			$(".filter-price__to").text(data.to);
